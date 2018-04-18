c9b508e05d8b6a13dd32b78cbfa76a632ae1fa9c
hackett91 <C16723031@mydit.ie>
2018-04-18 13:12:43 +0100

    finishing heap

--- heaps/Heap.java
+++ heaps/Heap.java
@@ -4,7 +4,7 @@ public class Heap{
     private int size;
 
     public Heap(int capacity){
-      heap = new int(capactity);
+      heap = new int[capacity];
     }
 
     public void insert(int value){
@@ -18,6 +18,71 @@ public class Heap{
       size++;
     }
 
+    public int delete(int index){
+      if(isEmpty()){
+        throw new IndexOutOfBoundsException("Heap is Empty");
+      }
+
+      int parent = getParent(index);
+      int deletedValue = heap[index];
+
+      heap[index] = heap[size-1];
+
+      if(index == 0 || heap[index] < heap[parent]){
+        fixHeapBelow(index, size - 1);
+      }
+      else
+      {
+        fixHeapAbove(index);
+      }
+      size--;
+
+      return deletedValue;
+
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex){
+      int childToSwap;
+
+      while(index<= lastHeapIndex){
+        int leftChild = getChild(index,true);
+        int rightChild = getChild(index,false);
+        //check to see if have children
+        if (leftChild <= lastHeapIndex){
+          // if only have left pick index
+            if(rightChild > lastHeapIndex){
+              childToSwap = leftChild;
+            }
+            //if have right pick index of largest to swap
+            else
+            {
+              childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+            }
+            //compare values
+            if(heap[index] < heap[childToSwap]){
+              int tmp = heap[index];
+              heap[index] = heap[childToSwap];
+              heap[childToSwap] = tmp;
+            }
+            else
+            {
+              break;
+            }
+            index = childToSwap;
+        }
+        else{
+            break;
+        }
+      }
+    }
+
+    public void printHeap(){
+      for(int i =0; i<size; i++){
+        System.out.print(heap[i]);
+        System.out.print(", ");
+      }
+        System.out.println();
+    }
     //sifting up till greater than parent or at root
     private void fixHeapAbove(int index){
       //new value entered
@@ -35,10 +100,20 @@ public class Heap{
     }
 
     public boolean isFull(){
-      return size == heap.length
+      return size == heap.length;
     }
 
     public int getParent(int index){
       return (index - 1) / 2;
+    }
+
+    public boolean isEmpty(){
+      return size == 0;
+    }
+
+    public int getChild(int index, boolean left){
+
+        return 2* index *(left ? 1 : 2);
+
     }
 }
--- heaps/Main.java
+++ heaps/Main.java
@@ -4,6 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+      Heap heap = new Heap(10);
+
+      heap.insert(80);
+      heap.insert(75);
+      heap.insert(68);
+      heap.insert(55);
+      heap.insert(40);
+      heap.insert(52);
+      heap.insert(67);
+
+      heap.printHeap();
+
+      heap.delete(2);
+
+      heap.printHeap();
+
+
+
     }
 
 
